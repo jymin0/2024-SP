f7217847c3aa27625dacb1f5b176377b6f97d3ad
danielseo77 <72370353+danielseo77@users.noreply.github.com>
2024-04-15 13:21:18 +0900

    Delete Schedule directory

--- Schedule/sep.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd C:/Users/danie/OneDrive/바탕 화면/server/sep
-
-C:/Users/danie/OneDrive/바탕 화면/server/sep/Scripts/activate
--- Schedule/serp.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, jsonify, request
-app = Flask(__name__)
-
-@app.route('/')
-def timetable():
-    return render_template("timetable.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)

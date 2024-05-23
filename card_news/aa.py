3dba73aa5a84a8bde62524c2cb75eda588e37da4
Minjaeyeong <49185059+jymin0@users.noreply.github.com>
2024-05-23 09:36:24 +0900

    Add files via upload

--- card_news/aa.py
+++ card_news/aa.py
@@ -16,8 +16,10 @@ def index():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM images")
     image_data = cur.fetchall()
+    print("Image Data:", image_data)  # 디버깅을 위해 출력
     cur.execute("SELECT * FROM videos")
     video_data = cur.fetchall()
+    print("Video Data:", video_data)  # 디버깅을 위해 출력
     return render_template('index.html', image_data=image_data, video_data=video_data)
 
 @app.route('/search', methods=['POST'])
@@ -26,8 +28,10 @@ def search():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM images WHERE title LIKE %s", ('%' + search_keyword + '%',))
     filtered_image_data = cur.fetchall()
+    print("Filtered Image Data:", filtered_image_data)  # 디버깅을 위해 출력
     cur.execute("SELECT * FROM videos WHERE title LIKE %s", ('%' + search_keyword + '%',))
     filtered_video_data = cur.fetchall()
+    print("Filtered Video Data:", filtered_video_data)  # 디버깅을 위해 출력
     return render_template('index.html', image_data=filtered_image_data, video_data=filtered_video_data)
 
 if __name__ == '__main__':
